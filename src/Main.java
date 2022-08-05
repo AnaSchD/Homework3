27e4d33afae3421e7ab0e2b093a7870f6fc633d4
Scherbakova <nastyasch0806@gmail.com>
2022-08-05 20:36:47 +0300

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        
+        int clientOSIos = 0;
+        int clientOSAndroid = 1;
+        if (clientOSIos ==0); {
+            System.out.println("Установите версию приложения iOS по ссылке");
+        }
+        if (clientOSAndroid == 1); {
+                System.out.println("Установите версию приложения для Android");
+            }
     }
 }
