ac275649d2c11b5a87fc3d16c3e0d65328402461
Scherbakova <nastyasch0806@gmail.com>
2022-08-05 23:31:50 +0300

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -2,16 +2,19 @@ public class Main {
     public static void main(String[] args) {
         //задача 1
         int clientOS = 1;
-        if (clientOS == 0); {
+        if (clientOS == 0)
+             {
             System.out.println("Установите версию приложения iOS по ссылке");
-        } if (clientOS == 1); {
+             }
+        if (clientOS == 1)
+            {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
             // задача 2
 
-        int clientDeviceYear = 2017;
+        int clientDeviceYear = 2014;
         if (clientOS == 1)
-            {
+                 {
             if (clientDeviceYear >= 2015)
                 {
                     System.out.println("Установите версию приложения для iOS по ссылке");
@@ -31,13 +34,23 @@ public class Main {
             System.out.println ("Установи облегченную версию приложения для Android по ссылке");
         }
               }
-        }
+
+        // задача 3
+
+        int year = 1904;
+
+        if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0)
+        {
+            System.out.println ("Год является високосным");
+        } else
+            {
+                System.out.println ("Год не является високосным");
+            }
 
 
 
 
 
-
-
+    }
 
         }
