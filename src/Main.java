742592f13a316ed3861b08559102a24f2f7593bf
Scherbakova <nastyasch0806@gmail.com>
2022-08-05 21:47:45 +0300

    Homework3

--- src/Main.java
+++ src/Main.java
@@ -7,9 +7,20 @@ public class Main {
         } if (clientOS == 1); {
                 System.out.println("Установите версию приложения для Android по ссылке");
             }
-    }
- // задача 2
-    int clientOS = 1;
-    int clientDeviceYear = 2017;
+            // задача 2
 
-}
+        int clientDeviceYear = 2017;
+        if (clientOS == 1); {
+            if (clientDeviceYear >= 2015); {
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            } if (clientDeviceYear < 2015); {
+                System.out.println ("Установи облегченную версию приложения для iOS по ссылке");
+            }
+        }{
+            if (clientDeviceYear >= 2015);
+            System.out.println("Установите версию приложения для Android по ссылке");
+            } if (clientDeviceYear < 2015); {
+                System.out.println("Установи облегченную версию приложения для Android по ссылке");
+            }
+        }
+        }
